c3b1fcd1a6d8e16a77c552b664eb6003d3c2764e
Tony Terra Nova <112224948+ItsTonyy@users.noreply.github.com>
2025-03-10 00:38:41 -0300

    style v2

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,24 @@ function App() {
 
         <div className="flex flex-col">
           <div className="flex flex-col">
-            <label htmlFor="fname" className="mr-4">First Name</label>
-            <input type="text" name="fname" id="fname" className="border " />
+            <label htmlFor="fname" className="text-lg">First Name</label>
+            <input type="text" name="fname" id="fname" className="text-xl mb-4 active:border-x-0 active:border-t-0 outline-none border active:border-black border-l-blue-500 border-r-blue-500 border-t-blue-500" />
           </div>
 
           <div className="flex flex-col">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" className="border " />
+            <label htmlFor="email" className="text-lg">Email</label>
+            <input type="email" name="email" id="email" className="text-xl mb-4 active:border-x-0 active:border-t-0 outline-none border active:border-black border-l-blue-500 border-r-blue-500 border-t-blue-500" />
+          </div>
+
+          <div className="flex flex-col">
+            <label htmlFor="fname" className="text-lg">Password</label>
+            <input type="password" name="password" id="password" className="text-xl active:border-x-0 active:border-t-0 outline-none border active:border-black border-l-blue-500 border-r-blue-500 border-t-blue-500"/>
           </div>
 
 
-          <input type="submit" value="enviar" className="bg-red-700 p-2 rounded-2xl cursor-pointer mt-8" />
+
+          <input type="submit" value="Submit" className="bg-black text-white shadow-2xl p-2 rounded-2xl cursor-pointer mt-8
+          active:scale-95 transition-transform duration-100" />
 
         </div>
       </div>
